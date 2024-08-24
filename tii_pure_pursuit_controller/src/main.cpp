51c0445d936984206a40bd8d9ea67879248c092c
lp-karim <karim.ziadi@lexxpluss.com>
2024-08-25 00:08:40 +0900

    syntax correction (semi-column)

--- tii_pure_pursuit_controller/src/main.cpp
+++ tii_pure_pursuit_controller/src/main.cpp
@@ -1,9 +1,12 @@
+#include <ros/ros.h>
+#include "pure_pursuit_controller/pure_pursuit_controller.h"
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "pure_pursuit_controller");
     ros::NodeHandle nh;
 
     PurePursuitController controller(nh);
 
-    ros::spin()
+    ros::spin();
     return 0;
 }
