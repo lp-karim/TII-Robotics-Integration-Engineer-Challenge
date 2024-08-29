e07ec9aeff919b0357962333754875b05fc3f6d3
lp-karim <karim.ziadi@lexxpluss.com>
2024-08-30 03:04:53 +0900

    removed commented code

--- tii_pure_pursuit_controller/src/main.cpp
+++ tii_pure_pursuit_controller/src/main.cpp
@@ -15,60 +15,6 @@
 #include <fstream>
 #include <sstream>
 
-// int main(int argc, char** argv) {
-//     ros::init(argc, argv, "path_tracking_node");
-//     ros::NodeHandle nh;
-    
-//     std::string waypoints_file = "/root/catkin_ws/src/POLARIS_GEM_e2-main/polaris_gem_drivers_sim/gem_pure_pursuit_sim/waypoints/wps.csv";
-//     nh.param<std::string>("waypoints_file", waypoints_file, waypoints_file);
-
-//     // Parameters for input and output topics, and new frame_id
-//     std::string input_topic = "/gem/velodyne_points";
-//     std::string output_topic = "/gem/velodyne_points";
-//     std::string new_frame_id = "velodyne_base_link";
-//     nh.param<std::string>("input_topic", input_topic, input_topic);
-//     nh.param<std::string>("output_topic", output_topic, output_topic);
-//     nh.param<std::string>("new_frame_id", new_frame_id, new_frame_id);
-
-//     ROS_INFO("Input Topic: %s", input_topic.c_str());
-//     ROS_INFO("Output Topic: %s", output_topic.c_str());
-//     ROS_INFO("New Frame ID: %s", new_frame_id.c_str());
-
-//     FrameIdRemapper remapper(nh, input_topic, output_topic, new_frame_id);
-
-//     // Pure Pursuit setup
-//     double lookahead_distance = 6.0;  // Can be parameterized
-//     double wheelbase = 1.5;  // Example value, adjust based on your robot
-
-//     nh.param<double>("lookahead_distance", lookahead_distance, lookahead_distance);
-//     nh.param<double>("wheelbase", wheelbase, wheelbase);
-
-//     // Pure Pursuit controller instance
-//     PurePursuit pure_pursuit(lookahead_distance, wheelbase, marker_pub);
-
-//     // Path Tracking Planner setup
-//     PathTrackingPlanner planner(nh);
-//     std::vector<Waypoint> global_path_ = planner.loadWaypointsFromCSV(waypoints_file);
-    
-//     ros::Rate loop_rate(10);  // 10 Hz
-//     while (ros::ok()) {
-//         if (!global_path_.empty()) {
-//             std::vector<Waypoint> transformed_path = planner.transformGlobalPathToVehicleFrame(global_path_, planner.getCurrentPosition());
-//             double steering_angle = pure_pursuit.computeSteeringAngle(planner.getCurrentPosition(), transformed_path);
-//             planner.publishPath(global_path_, path_pub_);
-
-//             geometry_msgs::Twist cmd_msg;
-//             cmd_msg.linear.x = 1.0;  // Example speed, adjust as needed
-//             cmd_msg.angular.z = steering_angle;
-
-//             cmd_pub_.publish(cmd_msg);
-//         }
-//         ros::spinOnce();
-//         loop_rate.sleep();
-//     }
-
-//     return 0;
-// }
 
 // Global variable for waypoints
 std::vector<Waypoint> global_path_;
