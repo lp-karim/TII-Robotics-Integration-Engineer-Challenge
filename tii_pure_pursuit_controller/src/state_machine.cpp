b7464a306a1ebd11f4a88412222702f4f989241d
lp-karim <karim.ziadi@lexxpluss.com>
2024-08-30 19:31:40 +0900

    added color to state machine log

--- tii_pure_pursuit_controller/src/state_machine.cpp
+++ tii_pure_pursuit_controller/src/state_machine.cpp
@@ -3,25 +3,19 @@
 #include "pure_pursuit_controller/path_tracking_planner.h"
 #include "pure_pursuit_controller/state_machine.h"
 
-// global variable
-// extern std::vector<Waypoint> global_path_;
 
-
-StateMachine::StateMachine(PurePursuit& controller, PathTrackingPlanner& planner, std::string& waypoints_file)
+StateMachine::StateMachine(PurePursuit& controller, PathTrackingPlanner& planner, std::string& waypoints_file, std::vector<Waypoint>& global_path_)
     : current_state_(VehicleState::Idle), controller_(controller), planner_(planner), waypoints_file_(waypoints_file)  {
 
         cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("/gem/cmd_vel", 10);
         path_pub_ = nh_.advertise<nav_msgs::Path>("/gem/path", 10);
         marker_pub_ = nh_.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+        remaing_waypoint = global_path_.size();
     }
 
 void StateMachine::run(std::vector<Waypoint>& global_path_) {
-    // global_path_ = planner_.loadWaypointsFromCSV(waypoints_file_);
-    ROS_INFO("Waypoints loaded: %lu", global_path_.size());
+    
     planner_.publishPath(global_path_, path_pub_);
-    // for (size_t i = 0; i < global_path_.size(); ++i) {
-    //         ROS_INFO("Waypoint %lu: x = %f, y = %f, yaw = %f", i, global_path_[i].x, global_path_[i].y, global_path_[i].yaw);
-    //     }
     switch (current_state_) {
         case VehicleState::Idle:
             handleIdleState(global_path_);
@@ -30,13 +24,16 @@ void StateMachine::run(std::vector<Waypoint>& global_path_) {
             handlePathFollowingState(global_path_);
             break;
         case VehicleState::Completed:
-            handleCompletedState();
+            handleCompletedState(global_path_);
             break;
     }
 }
 
 void StateMachine::handleIdleState(std::vector<Waypoint>& global_path_) {
-    ROS_INFO("State: Idle. Waiting for path...");
+    // ANSI escape code for Yellow
+    const std::string reset   = "\033[0m";
+    const std::string yellow  = "\033[33m";
+    ROS_INFO("%sState: Idle. Waiting for path...%s",yellow.c_str(), reset.c_str());
 
     // Transition to PathFollowing state if path is available
     if (!global_path_.empty()) {
@@ -46,39 +43,41 @@ void StateMachine::handleIdleState(std::vector<Waypoint>& global_path_) {
 }
 
 void StateMachine::handlePathFollowingState(std::vector<Waypoint>& global_path_) {
-    ROS_INFO("State: PathFollowing. Tracking the path...");
-
-    // Check if waypoint_index has reached the last waypoint
-    if (waypoint_index >= global_path_.size() - 1) {
-        ROS_INFO("Reached the last waypoint. Transitioning to Completed state.");
-        current_state_ = VehicleState::Completed;
-        return;
-    }
-
-    // Transform the global path into the vehicle's coordinate frame
-    std::vector<Waypoint> transformed_path = planner_.transformGlobalPathToVehicleFrame(global_path_, planner_.getCurrentPosition());
-    planner_.publishPath(global_path_, path_pub_);
-
+    // ANSI escape code for Blue
+    const std::string reset   = "\033[0m";
+    const std::string blue    = "\033[34m";
+    ROS_INFO("%sState: PathFollowing. Tracking the path...%s",blue.c_str(), reset.c_str());
+    
     // Compute the steering angle
-    std::pair<double, int> result = controller_.computeSteeringAngle(planner_.getCurrentPosition(), transformed_path);
+    std::pair<double, int> result = controller_.computeSteeringAngle(planner_.getCurrentPosition(), global_path_);
     double steering_angle = result.first;
     waypoint_index = result.second;
 
     // Publish the command
-    cmd_msg.linear.x = 2.0;  // 1.0
+    cmd_msg.linear.x = 3.0;  // 1.0
     cmd_msg.angular.z = steering_angle;
 
     cmd_pub_.publish(cmd_msg);
-
     // If the vehicle has reached the last waypoint, transition to Completed state
-    if (waypoint_index >= global_path_.size() - 1) {
-        ROS_INFO("Reached the last waypoint. Transitioning to Completed state.");
+    if (remaing_waypoint <  1) {
+        // ANSI escape code for Green
+        const std::string reset   = "\033[0m";
+        const std::string green   = "\033[32m";
+        ROS_INFO("%sReached the last waypoint. Transitioning to Completed state.%s",green.c_str(), reset.c_str());
         current_state_ = VehicleState::Completed;
+        return;
+    } else {
+        remaing_waypoint -= 1;
+        ROS_INFO("Remaining Waypoints: %d", remaing_waypoint);
     }
 }
 
-void StateMachine::handleCompletedState() {
-    ROS_INFO("State: Completed. Stopping The Vehicle.");
+void StateMachine::handleCompletedState(std::vector<Waypoint>& global_path_) {
+    global_path_.clear();
+    // ANSI escape code for Green
+    const std::string reset   = "\033[0m";
+    const std::string green   = "\033[32m";
+    ROS_INFO("%sState: Completed. Stopping The Vehicle.%s",green.c_str(), reset.c_str());
 
     //stopping the Vehicle
     waypoint_index = 0;
@@ -86,12 +85,6 @@ void StateMachine::handleCompletedState() {
     cmd_msg.angular.z = 0.0;
 
     cmd_pub_.publish(cmd_msg);
-}
-
-bool StateMachine::isPathCompleted() {
-    // This could be based on the distance to the last waypoint or some other condition
-    // Placeholder logic:
-    // return global_path_.size() == 1;  // Example condition
 }
 
 VehicleState StateMachine::getCurrentState() const {
